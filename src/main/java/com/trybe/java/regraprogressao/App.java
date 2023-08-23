202309b787a5a7de18d8199d08c88653f49eba42
Georgia <georgiab543@gmail.com>
2023-08-23 10:14:03 -0300

    feat: requisito 2

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -20,6 +20,8 @@ public class App {
 
     ArrayList<String> atividades = new ArrayList<>();
     ArrayList<Integer> pesos = new ArrayList<>();
+    ArrayList<Integer> notas = new ArrayList<>();
+    double sumNotas = 0
 
     for (int i = 1; i <= qntAtividade; i++) {
       System.out.println("Digite o nome da atividade " + i + ":");
@@ -27,6 +29,11 @@ public class App {
 
       System.out.println("Digite o peso da atividade " + i + ":");
       pesos.add(Integer.parseInt(scanner.nextLine()));
+
+      System.out.println("Digite a nota obtida para " + i + ":");
+      notas.add(Integer.parseInt(scanner.nextLine()));
+
+      sumNotas +=  pesos[i] *  notas[i];
     }
 
     System.out.println(
@@ -35,37 +42,3 @@ public class App {
     scanner.close();
   }
 }
-
-// package com.trybe.java.regraprogressao;
-
-// import java.util.Arrays;
-// import java.util.Scanner;
-
-// /**
-//  * App.
-//  */
-// public class App {
-
-//   /**
-//    * Metodo main.
-//    */
-//   public static void main(String[] args) {
-//     Scanner scanner = new Scanner(System.in);
-
-//     System.out.println("Digite a quantidade de atividades para cadastrar:");
-//     int qntAtividade = Integer.parseInt((scanner.nextLine()));
-//     int[] pesos = new int[qntAtividade];
-//     String[] atividades = new String[qntAtividade];
-//     for (int i = 1; i <= qntAtividade; i++) {
-//       System.out.println("Digite o nome da atividade " + i + ":");
-//       atividades[i - 1] = scanner.nextLine();
-//       System.out.println("Digite o peso da atividade " + i + ":");
-//       pesos[i - 1] = Integer.parseInt(scanner.nextLine());
-//       System.out.print("Digite a nota obtida para" + " " + atividades[i - 1] + ": ");
-//       float nota =  Float.parseFloat(scanner.nextLine());
-//     }
-//     System.out.println(
-//             "pesos:" + Arrays.toString(pesos) + "atividades:" + Arrays.toString(atividades));
-//     scanner.close();
-//   }
-// }
