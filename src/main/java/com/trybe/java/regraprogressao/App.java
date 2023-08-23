b3efb81b54e39291798e6b3ee3a0863d1dbc3a3a
Georgia <georgiab543@gmail.com>
2023-08-23 12:03:51 -0300

    feat: completo

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -15,23 +15,65 @@ public class App {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Digite a quantidade de atividades para cadastrar:");
-    int qntAtividade = Integer.parseInt(scanner.nextLine());
+    int numAct = readQuanty(scanner);
 
-    ArrayList<String> atividades = new ArrayList<>();
-    double pesos = 0;
+    double totalPoints = 0;
+    double totalWeights = 0;
 
-    for (int i = 1; i <= qntAtividade; i++) {
-      System.out.println("Digite o nome da atividade " + i + ":");
-      atividades.add(scanner.nextLine());
+    for (int i = 1; i <= numAct; i++) {
+      String nameAtc = readName(scanner, i);
+      double weightsAct = readWeightsAct(scanner, i);
+      double gradeAct = readGradeAct(scanner, nameAtc);
 
-      System.out.println("Digite o peso da atividade " + i + ":");
-      pesos += (Integer.parseInt(scanner.nextLine()));
+      totalWeights += weightsAct;
+      totalPoints += weightsAct * gradeAct;
+
+      System.out.println();
     }
 
-    System.out.println(
-        "pesos:" + pesos + "atividades:" + atividades.toString()
-    );
+    double notaFinal = finalGrade(totalPoints, totalWeights);
+
+    result(notaFinal);
+
     scanner.close();
+  }
+
+  private static int readQuanty(Scanner scanner) {
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    return scanner.nextInt();
+  }
+
+  private static String readName(Scanner scanner, int i) {
+    scanner.nextLine();
+    System.out.println("Digite o nome da atividade " + i + ":");
+    return scanner.nextLine();
+  }
+
+  private static double readWeightsAct(Scanner scanner, int i) {
+    System.out.println("Digite o peso da atividade " + i + ":");
+    return scanner.nextDouble();
+  }
+
+  private static double readGradeAct(Scanner scanner, String nameAtc) {
+    scanner.nextLine();
+    System.out.println("Digite a nota obtida para " + nameAtc + ":");
+    return scanner.nextDouble();
+  }
+
+  private static double finalGrade(double totalPoints, double totalWeights) {
+    return totalPoints / totalWeights;
+  }
+
+  private static void result(double notaFinal) {
+    System.out.println("Você alcançou " + notaFinal + "%!");
+
+    if (notaFinal >= 85) {
+      System.out.println("Parabéns! Você alcançou " + notaFinal + "%! E temos o prazer de"
+              + " informar que você obteve aprovação!");
+    } else {
+      System.out.println("Lamentamos informar que, com base na sua pontuação"
+              + " alcançada neste período, " + notaFinal + "%, você não atingiu a pontuação mínima"
+              + " necessária para sua aprovação.");
+    }
   }
 }
