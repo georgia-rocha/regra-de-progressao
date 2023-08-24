2800eda383d915d589ffa175994c0ee65bde36a5
Georgia <georgiab543@gmail.com>
2023-08-24 00:02:11 -0300

    feat: requisito 3 e correção do requisito 2

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -17,6 +17,7 @@ public class App {
 
     int numAct = readQuanty(scanner);
 
+    double totalPoints = 0;
     double totalWeights = 0;
 
     for (int i = 1; i <= numAct; i++) {
@@ -25,9 +26,12 @@ public class App {
       double gradeAct = readGradeAct(scanner, nameAtc);
 
       totalWeights += weightsAct;
+      totalPoints += weightsAct * gradeAct;
 
       System.out.println();
     }
+
+    double gradeFinal = finalGrade(totalPoints, totalWeights);
 
     result(gradeFinal);
 
@@ -54,5 +58,22 @@ public class App {
     scanner.nextLine();
     System.out.println("Digite a nota obtida para " + nameAtc + ":");
     return scanner.nextDouble();
+  }
+
+  private static double finalGrade(double totalPoints, double totalWeights) {
+    return totalPoints / totalWeights;
+  }
+
+  private static void result(double gradeFinal) {
+    System.out.println("Você alcançou " + gradeFinal + "%!");
+
+    if (gradeFinal >= 85) {
+      System.out.println("Parabéns! Você alcançou " + gradeFinal + "%! E temos o prazer de"
+              + " informar que você obteve aprovação!");
+    } else {
+      System.out.println("Lamentamos informar que, com base na sua pontuação"
+              + " alcançada neste período, " + gradeFinal + "%, você não atingiu a pontuação mínima"
+              + " necessária para sua aprovação.");
+    }
   }
 }
